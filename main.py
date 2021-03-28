5cf8a7f4119f0a86fa261dda32d368bd929d6bf7
Mikhail Prozorov <mikepro@alfamail.ru>
2021-03-29 01:11:00 +0300

    Added iter def to class animal()

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ class animal():
         self.name = name
         self.weight = weight
         self.life = 100
+        self.food = 0
 
     def feed(self, f_count):
-        self.weight += f_count
+        self.food += f_count
 
     def __iter__(self):
         return self
 
     def __next__(self):
         self.life -= (100 - self.food) / 5
+        self.weight *= 1 - (100 - self.food) / 500
         if self.life < 0:
             self.life = 0
         self.food = 0
@@ -63,7 +65,8 @@ class duck(eggs, animal):
 def main():
     duck1 = duck('Кряква', 3)
     print(duck1.type, duck1.name, duck1.weight)
-    duck1.feed(0.5)
+    duck1.feed(150)
+    next(duck1)
     print(duck1.type, duck1.name, duck1.weight)
 
 def cow_ascii():
